2bcaee5d762f6f7c924dec24bda6eca463f76404
dylanchen0114 <dylanchen0114@gmail.com>
2017-05-25 22:10:33 +0800

    pre-processing

--- script/pre-processing_1.py
+++ script/pre-processing_1.py
@@ -7,8 +7,8 @@ import joblib
 import numpy as np
 import pandas as pd
 
-train = pd.read_csv(os.path.join('../dataset', 'train.csv'))
-test = pd.read_csv(os.path.join('../dataset', 'test.csv'))
+train = pd.read_csv(os.path.join('../dataset', 'train.csv'), dtype='str')
+test = pd.read_csv(os.path.join('../dataset', 'test.csv'), dtype='str')
 
 instance_id = test['instanceID']
 test.drop('instanceID', inplace=True, axis=1)
@@ -17,19 +17,19 @@ all_data = pd.concat([train, test])
 print('All dataset shape:', all_data.shape)
 
 
-ad = pd.read_csv(os.path.join('../dataset', 'ad.csv'))
+ad = pd.read_csv(os.path.join('../dataset', 'ad.csv'), dtype='str')
 all_data = pd.merge(all_data, ad, how='left', on='creativeID')
 print('After left-join ad, shape:', all_data.shape)
 
-app_categories = pd.read_csv(os.path.join('../dataset', 'app_categories.csv'))
+app_categories = pd.read_csv(os.path.join('../dataset', 'app_categories.csv'), dtype='str')
 all_data = pd.merge(all_data, app_categories, how='left', on='appID')
 print('After left-join app_categories, shape:', all_data.shape)
 
-position = pd.read_csv(os.path.join('../dataset', 'position.csv'))
+position = pd.read_csv(os.path.join('../dataset', 'position.csv'), dtype='str')
 all_data = pd.merge(all_data, position, how='left', on='positionID')
 print('After left-join position, shape:', all_data.shape)
 
-user = pd.read_csv(os.path.join('../dataset', 'user.csv'))
+user = pd.read_csv(os.path.join('../dataset', 'user.csv'), dtype='str')
 all_data = pd.merge(all_data, user, how='left', on='userID')
 print('After left-join user, shape:', all_data.shape)
 
@@ -46,9 +46,9 @@ id_columns = ['creativeID', 'positionID', 'adID', 'camgaignID',
 
 # plot_utils.plot_correlation_map(all_data[id_columns])
 
-all_data['site_position'] = np.add(all_data.sitesetID.values.astype('str'), all_data.positionType.values.astype('str'))
-all_data['app_id_platform'] = np.add(all_data.appID.values.astype('str'), all_data.appPlatform.values.astype('str'))
-all_data['advertiser_app_id'] = np.add(all_data.advertiserID.values.astype('str'), all_data.appID.values.astype('str'))
+all_data['site_position'] = np.add(all_data.sitesetID.values, all_data.positionType.values)
+all_data['app_id_platform'] = np.add(all_data.appID.values, all_data.appPlatform.values)
+all_data['advertiser_app_id'] = np.add(all_data.advertiserID.values, all_data.appID.values)
 
 
 print('Saving ...')
