ab6118d4bf070a921e637041ba78702c07d063b6
dylanchen0114 <dylanchen0114@gmail.com>
2017-05-26 22:16:24 +0800

    add_category_counts.py

--- script/pre-processing_1.py
+++ script/pre-processing_1.py
@@ -6,9 +6,17 @@ from my_tools import plot_utils
 import joblib
 import numpy as np
 import pandas as pd
+from script import utils
 
-train = pd.read_csv(os.path.join('../dataset', 'train.csv'), dtype='str')
-test = pd.read_csv(os.path.join('../dataset', 'test.csv'), dtype='str')
+from pyspark import SparkContext, sql
+
+os.environ['PYSPARK_SUBMIT_ARGS']="--master local[*] pyspark-shell"
+sc = SparkContext('local[*]')
+spark = sql.SparkSession.builder.appName("").getOrCreate()
+
+
+train = pd.read_csv(os.path.join('../dataset', 'train.csv'))
+test = pd.read_csv(os.path.join('../dataset', 'test.csv'))
 
 instance_id = test['instanceID']
 test.drop('instanceID', inplace=True, axis=1)
@@ -17,19 +25,19 @@ all_data = pd.concat([train, test])
 print('All dataset shape:', all_data.shape)
 
 
-ad = pd.read_csv(os.path.join('../dataset', 'ad.csv'), dtype='str')
+ad = pd.read_csv(os.path.join('../dataset', 'ad.csv'))
 all_data = pd.merge(all_data, ad, how='left', on='creativeID')
 print('After left-join ad, shape:', all_data.shape)
 
-app_categories = pd.read_csv(os.path.join('../dataset', 'app_categories.csv'), dtype='str')
+app_categories = pd.read_csv(os.path.join('../dataset', 'app_categories.csv'))
 all_data = pd.merge(all_data, app_categories, how='left', on='appID')
 print('After left-join app_categories, shape:', all_data.shape)
 
-position = pd.read_csv(os.path.join('../dataset', 'position.csv'), dtype='str')
+position = pd.read_csv(os.path.join('../dataset', 'position.csv'))
 all_data = pd.merge(all_data, position, how='left', on='positionID')
 print('After left-join position, shape:', all_data.shape)
 
-user = pd.read_csv(os.path.join('../dataset', 'user.csv'), dtype='str')
+user = pd.read_csv(os.path.join('../dataset', 'user.csv'))
 all_data = pd.merge(all_data, user, how='left', on='userID')
 print('After left-join user, shape:', all_data.shape)
 
@@ -49,6 +57,25 @@ id_columns = ['creativeID', 'positionID', 'adID', 'camgaignID',
 all_data['site_position'] = np.add(all_data.sitesetID.values, all_data.positionType.values)
 all_data['app_id_platform'] = np.add(all_data.appID.values, all_data.appPlatform.values)
 all_data['advertiser_app_id'] = np.add(all_data.advertiserID.values, all_data.appID.values)
+
+
+# using group-by to check encode corr variables
+# plot_utils.plot_cate_bar(all_data.ix[all_data['label'] != -1], x='test', y='label', estimator=np.mean)
+
+
+# installed app category
+print('Calculating installed app category ...')
+user_installedapps = spark.read.csv(os.path.join('../dataset', 'user_installedapps.csv'), header=True)
+app_categories = spark.read.csv(os.path.join('../dataset', 'app_categories.csv'), header=True)
+category_count = user_installedapps.join(app_categories, 'appID', 'left_outer').groupBy(['userID', 'appCategory']).count().toPandas()
+
+
+
+
+
+
+
+
 
 
 print('Saving ...')
