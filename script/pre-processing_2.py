026d47799d5ae06aa00f99d2b19a616dece71418
dylanchen0114 <dylanchen0114@gmail.com>
2017-06-01 00:21:40 +0800

    add_category_counts.py

--- script/pre-processing_2.py
+++ script/pre-processing_2.py
@@ -4,25 +4,24 @@ import os
 
 import joblib
 
-import utils
+from script import utils
 
 
 def xgrfir():
 
-    all_data = joblib.load(os.path.join('../processed', 'all_data_p1'))
+    all_data = joblib.load(os.path.join('../processed', 'all_data_p2'))
     all_data.drop(['conversionTime', 'clickTime'], inplace=True, axis=1)
 
     presets = {
-        'xgb-tst2': {
+        'xgb-tst4': {
             'model': utils.Xgb({
-                'max_depth': 7,
-                'eta': 0.0175,
-                'colsample_bytree': 0.7135,
-                'min_child_weight': 2.2,
+                'max_depth': 10,
+                'eta': 0.15,
+                'colsample_bytree': 0.65,
+                'min_child_weight': 5,
+                'subsample': 0.9,
                 'early_stopping_rounds': 50
-            }, n_iter=4100),
-            'param_grid': {'colsample_bytree': [0.2, 1.0]},
-        }
+            }, n_iter=500)}
     }
 
     train_x = all_data.ix[all_data['click_day'] < 31, :].drop('label', axis=1)
@@ -34,7 +33,7 @@ def xgrfir():
     # high interaction by xgbfir
 
     print('xgbfi for feature interaction ...')
-    presets['xgb-tst2']['model'].fit(X_train=train_x, y_train=train_y, feature_names=feature_names, name='xgb-tst2', xgbfir_tag=1)
+    presets['xgb-tst4']['model'].fit(X_train=train_x, y_train=train_y, feature_names=feature_names, name='xgb', xgbfir_tag=1, xgbfir_name='tst4')
 
 
 if __name__ == '__main__':
